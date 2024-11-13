be314de5ef5108b6e4915f7f923fedf66353a4e3
Alley Hector <alley@chaninicholas.com>
2024-11-12 17:19:33 -0800

    Add loading indictor and some try/catching into Recipe component

--- components/Recipes.tsx
+++ components/Recipes.tsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from 'react'
 import { View, Text } from './Themed'
-import { Image, TouchableOpacity, Linking } from 'react-native'
+import {
+  Image,
+  TouchableOpacity,
+  Linking,
+  ActivityIndicator,
+} from 'react-native'
 import { searchRecipe } from '@/API/RecipesAPI'
 import { RecipeProps, RecipeSearchResponse } from '@/types/edamam'
 import {
@@ -16,13 +21,21 @@ import {
 const Recipes = ({ query }: RecipeProps) => {
   // State to store the fetched recipe data
   const [recipes, setRecipes] = useState<RecipeSearchResponse[]>([])
+  const [loading, setLoading] = useState<boolean>(false)
 
   // Function to fetch the recipe based on the query
   const fetchRecipe = async ({ query }: RecipeProps) => {
-    const data = await searchRecipe({ query })
+    setLoading(true)
+    try {
+      const data = await searchRecipe({ query })
 
-    if (data) {
-      setRecipes(data.hits.slice(0, 2))
+      if (data) {
+        setRecipes(data.hits.slice(0, 2))
+      }
+    } catch (error) {
+      console.error('Error fetching recipes:', error)
+    } finally {
+      setLoading(false)
     }
   }
 
@@ -35,26 +48,35 @@ const Recipes = ({ query }: RecipeProps) => {
 
   return (
     <View style={[column, card, backgroundColorVar1]}>
-      {recipes &&
-        recipes.map((recipe, index) => (
-          <View style={[row, backgroundColorVar1]} key={index}>
-            <View>
-              <Image source={{ uri: recipe.recipe.image }} style={apiImage} />
-            </View>
-            <View style={[apiTextContainer, backgroundColorVar1]}>
-              <TouchableOpacity
-                onPress={() => Linking.openURL(recipe.recipe.url)}
-              >
-                <Text style={apiTitle}>{recipe.recipe.label}</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={() => Linking.openURL(recipe.recipe.url)}
-              >
-                <Text>View Recipe</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        ))}
+      {loading ? (
+        <ActivityIndicator size='large' />
+      ) : (
+        <>
+          {recipes &&
+            recipes.map((recipe, index) => (
+              <View style={[row, backgroundColorVar1]} key={index}>
+                <View>
+                  <Image
+                    source={{ uri: recipe.recipe.image }}
+                    style={apiImage}
+                  />
+                </View>
+                <View style={[apiTextContainer, backgroundColorVar1]}>
+                  <TouchableOpacity
+                    onPress={() => Linking.openURL(recipe.recipe.url)}
+                  >
+                    <Text style={apiTitle}>{recipe.recipe.label}</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    onPress={() => Linking.openURL(recipe.recipe.url)}
+                  >
+                    <Text>View Recipe</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            ))}
+        </>
+      )}
     </View>
   )
 }
