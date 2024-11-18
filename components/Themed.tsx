59bad6c7eedf7ffca56fb69748777d91fc08c40b
Alley Hector <alley@chaninicholas.com>
2024-11-17 23:04:49 -0800

    RM missed imports

--- components/Themed.tsx
+++ components/Themed.tsx
@@ -7,7 +7,6 @@ import { Text as DefaultText, View as DefaultView } from 'react-native'
 
 import Colors from '@/constants/Colors'
 import { useColorScheme } from './useColorScheme'
-import { MarkdownStyles } from '@/types/contentful'
 
 type ThemeProps = {
   lightColor?: string
@@ -16,7 +15,6 @@ type ThemeProps = {
 
 export type TextProps = ThemeProps & DefaultText['props']
 export type ViewProps = ThemeProps & DefaultView['props']
-export type ExtendedMarkdownProps = ThemeProps & MarkdownStyles
 
 export function useThemeColor(
   props: { light?: string; dark?: string },
