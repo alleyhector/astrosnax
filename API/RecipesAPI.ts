829ab2231e67297c1e28c1c8973008751c98e263
Alley Hector <alley@chaninicholas.com>
2024-11-24 15:33:02 -0800

    I think this is the easiest way to use cached data as a fallback when request fails

--- API/RecipesAPI.ts
+++ API/RecipesAPI.ts
@@ -1,5 +1,6 @@
 import { RecipeProps } from '@/types/edamam'
 import axios from 'axios'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const appId = process.env.EXPO_PUBLIC_EDAMAM_ID
 if (!appId) {
@@ -23,13 +24,28 @@ export const searchRecipe = async ({ query, cuisineType }: RecipeProps) => {
 
   try {
     const response = await axios.get(url, { params })
+    const CACHE_KEY = `edamam-${query}`
+    // Store the fetched data in AsyncStorage
+    await AsyncStorage.setItem(CACHE_KEY, JSON.stringify(response.data))
+    console.log('ASYNC CACHE KEY: ', AsyncStorage.getItem(CACHE_KEY))
     return response.data
   } catch (error) {
     console.error('Error fetching recipe data:', error)
+
+    // Attempt to retrieve cached data
+    const cachedData = await AsyncStorage.getItem(`edamam-${query}`)
+    console.log('CACHED DATA: ', cachedData)
+    if (cachedData) {
+      console.warn('Falling back to cached recipe data.')
+
+      return JSON.parse(cachedData)
+    }
+
+    // If no cached data is available, throw the error
     if (axios.isAxiosError(error)) {
       throw new Error(`Error fetching recipe data: ${error.message}`)
     } else {
-      throw new Error('Error fetching recipe data')
+      throw new Error('Error fetching recipe data and no cached data available')
     }
   }
 }
