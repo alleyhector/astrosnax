7bf483643d1c2a0e268803f5d716a993c0691ec2
Alley Hector <alley@qpdx.com>
2026-06-19 15:48:00 -0700

    Cursor tried a new approach to the ios compatibility issue

--- plugins/withIosFmtFix.js
+++ plugins/withIosFmtFix.js
@@ -3,16 +3,21 @@ const { mergeContents } = require('@expo/config-plugins/build/utils/generateCode
 const fs = require('fs')
 const path = require('path')
 
-const TAG = 'withIosFmtFix'
+const TAG = 'withIosFmtFix-v2'
 
 // fmt 11.0.2 (bundled in RN 0.79) fails on Xcode 26.4 / Apple Clang 21 due to
-// stricter consteval rules. Compiling the fmt pod as C++17 skips that path.
+// stricter consteval rules. Patch base.h after pod install; build flags alone
+// do not stick because fmt's podspec xcconfig keeps c++20.
 const fmtFixRuby = `
-  installer.pods_project.targets.each do |target|
-    next unless target.name == 'fmt'
-
-    target.build_configurations.each do |config|
-      config.build_settings['CLANG_CXX_LANGUAGE_STANDARD'] = 'c++17'
+  fmt_base = File.join(installer.sandbox.root, 'fmt', 'include', 'fmt', 'base.h')
+  if File.exist?(fmt_base)
+    content = File.read(fmt_base)
+    unless content.include?('Xcode 26 workaround')
+      patched = content.gsub(/^# define FMT_USE_CONSTEVAL 1$/, '# define FMT_USE_CONSTEVAL 0')
+      if patched != content
+        File.chmod(0644, fmt_base)
+        File.write(fmt_base, patched)
+      end
     end
   end
 `.trim()
@@ -30,7 +35,14 @@ function withIosFmtFix(config) {
         return config
       }
 
-      const contents = fs.readFileSync(podfilePath, 'utf8')
+      let contents = fs.readFileSync(podfilePath, 'utf8')
+
+      // Drop the previous generated block so updated workaround code is applied.
+      contents = contents.replace(
+        /# @generated begin withIosFmtFix[^\n]*[\s\S]*?# @generated end withIosFmtFix\n?/g,
+        '',
+      )
+
       const result = mergeContents({
         tag: TAG,
         src: contents,
